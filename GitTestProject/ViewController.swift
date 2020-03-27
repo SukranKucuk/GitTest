1b80d87582162aa0ac063293c34548f171696b1e
Ali KÜÇÜK <alikucukcc@gmail.com>
2020-03-27 16:16:27 +0300

    second comment

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-print("git")
+        print("git")
+        print("git")
+        print("git")
+        print("git")
+        
     }
-
-
+    
+    
 }
 
