aea14f0b704ac17922479d78ed89cfba2cad26cc
Ali KÜÇÜK <alikucukcc@gmail.com>
2020-03-27 16:13:20 +0300

    first commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by ALİ KÜÇÜK on 27.03.2020.
+//  Copyright © 2020 Şükran Küçük. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("git")
+    }
+
+
+}
+
